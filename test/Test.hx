5fdc6c6384f0e35e9004ecd1aad7f98b9bfb361e
wartman <faxmaster@gmail.com>
2018-12-19 14:49:23 -0600

    removed element from this repo

--- test/Test.hx
+++ test/Test.hx
@@ -2,11 +2,6 @@ import js.html.InputElement;
 import js.html.Event;
 import js.Browser;
 import scout.html.Api.html;
-import scout.element.ScoutElement;
-
-import todo.model.Todo;
-import todo.view.TodoItem;
-import todo.view.TodoList;
 
 using scout.html.Renderer;
 
@@ -19,7 +14,6 @@ class Test {
       </header>
     ');
     var input = (
-      todos:Array<Todo>,
       initialValue:String, 
       handleClick:(e:Event)->Void
     ) -> html('
@@ -27,12 +21,8 @@ class Test {
       <div id="display"></div>
       <input id="target" class="test" name="foo" value="${initialValue}" />
       <button on:click="${handleClick}">Change</button>
-      <todo-list .todos="${todos}"></todo-list>
     ');
-    input([
-      new Todo('0', 'stuff', true),
-      new Todo('1', 'other stuff', false)
-    ], 'foo', e -> {
+    input('foo', e -> {
       var value:InputElement = cast Browser.document.getElementById('target');
       showFoo(value.value);
     }).render(Browser.document.getElementById('root'));
